52cc4028763886804c58727fb444e6d04e825666
FAL <falworks.contact@gmail.com>
2024-11-27 00:42:05 +0900

    fix Font-like types

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,21 @@
 import type { default as _TTFFont } from "./TTFFont.js";
-import type WOFFFont from "./WOFFFont.js";
-import type WOFF2Font from "./WOFF2Font.js";
+import type { default as _WOFFFont } from "./WOFFFont.js";
+import type { default as _WOFF2Font } from "./WOFF2Font.js";
 import type TrueTypeCollection from "./TrueTypeCollection.js";
 import type DFont from "./DFont.js";
 import type GlyphInfo from "./opentype/GlyphInfo.js";
 import type ShapingPlan from "./opentype/ShapingPlan.js";
 
-export type TTFFont = _TTFFont &
-  Record<string, object> & {
-    cff: any;
-    "OS/2": { sFamilyClass: number };
-    head: { macStyle: { italic: boolean } };
-    post: { isFixedPitch: boolean };
-  };
+type TTFFontExpansion = Record<string, object> & {
+  cff?: any;
+  "OS/2"?: { sFamilyClass: number };
+  head: { macStyle: { italic: boolean } };
+  post?: { isFixedPitch: boolean };
+};
+
+export type TTFFont = _TTFFont & TTFFontExpansion;
+export type WOFFFont = _WOFFFont & TTFFontExpansion;
+export type WOFF2Font = _WOFF2Font & TTFFontExpansion;
 export type Font = TTFFont | WOFFFont | WOFF2Font;
 export type FontCollection = TrueTypeCollection | DFont;
 
