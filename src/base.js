7ec3319e850b0fd8c1a2c4727c0b65d9d7aca1da
FAL <falworks.contact@gmail.com>
2024-11-26 15:58:48 +0900

    minor

--- src/base.js
+++ src/base.js
@@ -1,19 +1,25 @@
 // @ts-check
 
 // @ts-ignore
-import {DecodeStream} from 'restructure';
+import { DecodeStream } from 'restructure';
+
+// -----------------------------------------------------------------------------
 
 export let logErrors = false;
+
+// -----------------------------------------------------------------------------
 
 let formats = [];
 export function registerFormat(format) {
   formats.push(format);
-};
+}
+
+// -----------------------------------------------------------------------------
 
 /**
  * @param {ArrayBufferView} buffer
  * @param {string} [postscriptName]
- * @return {import("./types").Font | import("./types").FontCollection} 
+ * @return {import("./types").Font | import("./types").FontCollection}
  */
 export function create(buffer, postscriptName) {
   for (let i = 0; i < formats.length; i++) {
@@ -29,11 +35,15 @@ export function create(buffer, postscriptName) {
   }
 
   throw new Error('Unknown font format');
-};
+}
+
+// -----------------------------------------------------------------------------
 
 export let defaultLanguage = 'en';
 export function setDefaultLanguage(lang = 'en') {
   defaultLanguage = lang;
-};
+}
 
-export { default as DefaultShaper} from './opentype/shapers/DefaultShaper';
+// -----------------------------------------------------------------------------
+
+export { default as DefaultShaper } from './opentype/shapers/DefaultShaper';
