96d2a377a403f9b73de873246654b312dd79a77c
FAL <contact@fal-works.com>
2025-11-13 15:36:55 +0900

    remove WOFF2 tests

--- test/index.js
+++ test/index.js
@@ -30,9 +30,6 @@ describe('fontkit', function () {
 
     font = fontkit.openSync(new URL('data/NotoSans/NotoSans.dfont', import.meta.url), 'NotoSans');
     assert.equal(font.type, 'TTF');
-
-    font = fontkit.openSync(new URL('data/SourceSansPro/SourceSansPro-Regular.woff2', import.meta.url));
-    assert.equal(font.type, 'WOFF2');
   });
 
   it('should open fonts lacking PostScript name', function () {
